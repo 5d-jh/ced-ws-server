bc02d3a442041fdb203c8feaa44643458900f56c
이정현 <jeonghyun0126@outlook.com>
2019-11-05 14:01:48 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const { server: WebSocketServer } = require('websocket');
+const http = require('http');
+
+const httpServer = http.createServer((request, response) => {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+httpServer.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+const websocketServer = new WebSocketServer({ httpServer });
+
+websocketServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
