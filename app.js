a3348709d9aa977e5f0dbd04bd14e9e26b0f295b
이정현 <jeonghyun0126@outlook.com>
2019-11-05 14:27:38 +0900

    프로토콜 이슈 수정

--- app.js
+++ app.js
@@ -6,33 +6,24 @@ const httpServer = http.createServer((request, response) => {
     response.writeHead(404);
     response.end();
 });
-httpServer.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+httpServer.listen(33333, () => {
+    console.log((new Date()) + ' Server is listening on port 33333');
 });
 
 const websocketServer = new WebSocketServer({ httpServer });
 
-websocketServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-    
-    var connection = request.accept('echo-protocol', request.origin);
+websocketServer.on('request', request => {
+    const connection = request.accept();
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+    
+    connection.on('message', message => {
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+            connection.sendUTF('pong');
         }
     });
-    connection.on('close', function(reasonCode, description) {
+
+    connection.on('close', (reasonCode, description) => {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
